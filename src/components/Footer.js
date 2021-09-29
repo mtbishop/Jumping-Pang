8ed24dd85e08f801e0791d626ec71b16f5a61e59
mtbishop <bishopmatthew76@gmail.com>
2021-09-28 19:44:41 -0500

    adjusted footer contents and spacing

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,11 +1,49 @@
+import '../style/FooterStyle.css';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+
 function Footer() {
   return (
     <div>
-      <nav id="homepageFooter" className="navbar fixed-bottom">
-        <a className="navbar-brand" href="/">
-          Fixed bottom
-        </a>
-      </nav>
+      <Container>
+        <Row>
+          <nav id="homepageFooter" className="navbar fixed-bottom">
+            <Col>
+              <div className="footerColumns">
+                <h4>Info</h4>
+                <p>Formats</p>
+                <p>Compression</p>
+                <p>Pricing</p>
+                <p>FAQ</p>
+                <p>Status</p>
+              </div>
+            </Col>
+            <Col>
+              <div className="footerColumns">
+                <h4>Resources</h4>
+                <p>Dev API</p>
+                <p>Tools</p>
+                <p>Blog</p>
+              </div>
+            </Col>
+            <Col>
+              <div className="footerColumns">
+                <h4>Company</h4>
+                <p>About Us</p>
+                <p>Sustainability</p>
+                <p>Terms of Service</p>
+                <p>Privacy</p>
+              </div>
+            </Col>
+            <Col>
+              <div className="footerColumns">
+                <h4>SUB SECTION BELOW VVV</h4>
+              </div>
+            </Col>
+          </nav>
+        </Row>
+      </Container>
     </div>
   );
 }
